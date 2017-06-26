f3065d9b55fb7ad65e96c6c3e1b800e6d26f5e8b
ChallengerCY <790597004@qq.com>
2017-06-26 17:58:52 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   CSharpBase/CSharpBase.sln
    	new file:   CSharpBase/CSharpBase.v12.suo
    	new file:   CSharpBase/CSharpBase/App.config
    	new file:   CSharpBase/CSharpBase/CSharpValueType.csproj
    	new file:   CSharpBase/CSharpBase/Demo.cs
    	new file:   CSharpBase/CSharpBase/Properties/AssemblyInfo.cs
    	new file:   CSharpBase/CSharpBase/bin/Debug/CSharpBase.exe
    	new file:   CSharpBase/CSharpBase/bin/Debug/CSharpBase.exe.config
    	new file:   CSharpBase/CSharpBase/bin/Debug/CSharpBase.pdb
    	new file:   CSharpBase/CSharpBase/bin/Debug/CSharpBase.vshost.exe
    	new file:   CSharpBase/CSharpBase/bin/Debug/CSharpBase.vshost.exe.config
    	new file:   CSharpBase/CSharpBase/obj/Debug/CSharpBase.csproj.FileListAbsolute.txt
    	new file:   CSharpBase/CSharpBase/obj/Debug/CSharpBase.csprojResolveAssemblyReference.cache
    	new file:   CSharpBase/CSharpBase/obj/Debug/CSharpBase.exe
    	new file:   CSharpBase/CSharpBase/obj/Debug/CSharpBase.pdb
    	new file:   CSharpBase/CSharpBase/obj/Debug/CSharpValueType.csproj.FileListAbsolute.txt
    	new file:   CSharpBase/CSharpBase/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    	new file:   CSharpBase/CSharpBase/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs
    	new file:   CSharpBase/CSharpBase/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs
    	new file:   CSharpBase/CSharpBase/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs

---
+++ CSharpBase/CSharpBase/Demo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBase
+{
+    //枚举类型
+    //枚举类型目的是限定变量的可能性
+    //枚举类型默认对应的是整形，也可以指定类型
+    enum Days : byte {Monday,Tuesday,Wenesday,Thursday,Friday,Saturday,Sunday}
+
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+                //C#的值类型
+               //C#有三种类型，值类型，引用类型，指针类型(在不安全模式下用到)
+              //值类型存储在栈上面，引用类型是在栈上面存储一个引用，在内存的对上面存储值
+             //所有的值类型继承自System.ValueType,System.ValueType继承自System.Object（基类）
+            int value = 0;
+            int value_2 = new int();
+            bool a = true;
+
+            Person people=new Person();
+            people.age = 10;
+            //用var定义的变量支持任何类型
+            var day = Days.Friday;
+
+            Console.WriteLine(people.age);
+            Console.WriteLine(Days.Monday);
+            Console.WriteLine(day);
+            Console.ReadLine();        
+        }
+        //struct类型
+        //目的是打包封装一个数据集
+        struct Person
+        {
+            //C#中有四种访问修饰符
+            //public 对外都是可见的 private 私有的 internal在命名空间内是可以用的 protected 只有继承类才能使用
+            public int age;
+            private string name;
+            internal string fname;
+            //protected string lastname;         
+        }
+    }
+}
